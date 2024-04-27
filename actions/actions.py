d7c795284c4ecb51b1ba1b612a441ad1c31e19cd
koh3n <kohenlee1234@gmail.com>
2024-04-27 00:38:58 -0700

    scheduling and email chat functionality

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,21 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# custom_actions.py
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ClearSlotAction(Action):
+    def name(self) -> Text:
+        return "action_clear_email"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Clear the slot named 'slot_name'
+        return [
+            SlotSet("email", None),
+            SlotSet("body", None)
+            ]
